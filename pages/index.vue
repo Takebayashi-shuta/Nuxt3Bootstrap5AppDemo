fe6d4d6793b992800a9ecf31c60f5e06779f6351
Takebayashi-shuta <146409888+Takebayashi-shuta@users.noreply.github.com>
2023-10-18 14:48:34 +0000

    1018days

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>サイトのタイトル</title>
+    <title>Mycanvas</title>
     <meta name="description" content="サイトの説明文">
     <link rel="stylesheet" href="styles.css">
 </head>
@@ -19,7 +19,53 @@
 
 
     <main>
-       <h1>aurhgarui</h1>
+       <canvas id= mycanvas width= 400 height= 400></canvas>
+
+       <script>
+       var canvas = document.getElementById("myCanvas");
+        var context = canvas.getContext("2d");
+
+        // マウスがクリックされたかどうかのフラグ
+        var isDrawing = false;
+
+        // 描画の開始座標
+        var lastX = 0;
+        var lastY = 0;
+
+        // 描画スタイルの設定
+        context.strokeStyle = "#000";  // 描画の色
+        context.lineWidth = 2;         // 線の太さ
+
+        // マウスボタンが押されたときの処理
+        canvas.addEventListener("mousedown", function(e) {
+            isDrawing = true;
+            [lastX, lastY] = [e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop];
+        });
+
+        // マウスが移動したときの処理
+        canvas.addEventListener("mousemove", function(e) {
+            if (!isDrawing) return;  // マウスボタンが離れている場合は処理しない
+
+            // 線を描画
+            context.beginPath();
+            context.moveTo(lastX, lastY);
+            [lastX, lastY] = [e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop];
+            context.lineTo(lastX, lastY);
+            context.stroke();
+        });
+
+        // マウスボタンが離れたときの処理
+        canvas.addEventListener("mouseup", function() {
+            isDrawing = false;
+        });
+
+        // マウスがCanvasを離れたときの処理
+        canvas.addEventListener("mouseout", function() {
+            isDrawing = false;
+        });
+       
+       </script>
+
     </main>
 
 
